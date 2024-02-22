08ed3b15c99c1058ea7d6cc162d14325831738ae
124norellana@frhsd.com <124norellana@frhsd.com>
2024-02-22 08:25:11 -0500

    Lesson 4 Completed

--- Intro-to-Vue-3-master/main.js
+++ Intro-to-Vue-3-master/main.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
-            url: 'https://www.vuemastery.com/'
+            invetory: 100,
+            onSale: true
         }
     }
 })
